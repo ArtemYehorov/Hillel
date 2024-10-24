2e265a91712ef9432267d1e8e020fb2bad2a1ece
artem <artemyehorov@gmial.com>
2024-10-24 19:57:14 +0200

    first commit

--- test.py
+++ test.py
@@ -1,2 +1,9 @@
-print('hello')
-print('Papa')
+print('Введите целое 4-х значное число: ')
+userinput = input()
+
+usernumber = int(userinput)
+
+print(usernumber // 1000)
+print(usernumber // 100 % 10 )
+print(usernumber // 10 % 10 )
+print(usernumber % 10 )
