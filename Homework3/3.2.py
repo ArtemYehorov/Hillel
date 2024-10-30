b60e0488829e90624b8f319fdc3fa690fc3997f2
artem <artemyehorov@gmial.com>
2024-10-30 15:34:21 +0100

    homework3.2

--- Homework3/3.2.py
+++ Homework3/3.2.py
@@ -4,7 +4,8 @@ print(_list1)
 
 if len(_list1) > 1:
     _list1.insert(0,_list1.pop(-1))
-     #_list1.pop(-1) Подскажите пожалуйста как будет правильнее
+    #_list1.insert(0, _list1[-1]) Подскажите пожалуйста как будет правильнее
+    #_list1.pop(-1)
 
 print(_list1)
 
