0baeaca2eb23b6f1d9a28e2bfc8562273f517452
artem <artemyehorov@gmial.com>
2024-10-30 15:33:08 +0100

    homework3.3

--- Homework3/3.2.py
+++ Homework3/3.2.py
@@ -3,7 +3,8 @@ _list1 = [1,2,3,4,]
 print(_list1)
 
 if len(_list1) > 1:
-    _list1.insert(0,_list1[-1])
-    _list1.pop(-1)
+    _list1.insert(0,_list1.pop(-1))
+     #_list1.pop(-1) Подскажите пожалуйста как будет правильнее
 
 print(_list1)
+
