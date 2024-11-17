a2a7f40d40600c23251178be23121a3813a6ccf7
artem <artemyehorov@gmial.com>
2024-11-17 20:10:29 +0100

    7.1

---
+++ Homework7/7.1.py
@@ -0,0 +1,7 @@
+def say_hi(name, age):
+    result = f"Hi. My name is {name} and I'm {age} years old"
+    return result
+
+print(say_hi("Artem", 20))
+print(say_hi("Sasha", 35))
+print(say_hi("Petya", 12))
