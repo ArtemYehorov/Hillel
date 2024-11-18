6d7f6307c550990c2d69a898322bee772f012e86
artem <artemyehorov@gmial.com>
2024-11-18 22:29:58 +0100

    7.4 remade

--- Homework7/7.4.py
+++ Homework7/7.4.py
@@ -2,10 +2,7 @@ def common_elements():
     list1 = [x for x in range(0, 100, 3)]
     list2 = [x for x in range(0, 100, 5)]
 
-    print(list1)
-    print(list2)
-
-    common = list(set(list1) & set(list2))
+    common = set(set(list1) & set(list2))
     return common
 
 print(common_elements())
