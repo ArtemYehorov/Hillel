d1e2003a35f880d48027df5a274942fe3e5069cb
artem <artemyehorov@gmial.com>
2024-11-25 15:02:55 +0100

    9.1

--- Homework9/9.1.py
+++ Homework9/9.1.py
@@ -12,4 +12,5 @@ def popular_words(text: str, words: list):
 
    return counting_words
 
-print(popular_words("He said she said that we said everything was said already.",['he', 'she', 'said', 'was']))
+assert popular_words('''When I was One I had just begun When I was Two I was nearly new ''', ['i', 'was', 'three', 'near']) == { 'i': 4, 'was': 3, 'three': 0, 'near': 0 }, 'Test1'
+print('OK')
