9819dfffa549ca1433760016407dd948f5fe6cea
artem <artemyehorov@gmial.com>
2024-12-16 17:15:59 +0100

    14.2

---
+++ Homework13/projectGroup/group.py
@@ -0,0 +1,29 @@
+from student import Student
+from exceptions import InvalidStudentAdd
+
+class Group:
+
+    def __init__(self, number):
+        self.number = number
+        self.group = set()
+
+    def add_student(self, student):
+        if len(self.group) >= 10:
+            raise InvalidStudentAdd("Студентов в группе не может быть больше 10")
+        if isinstance(student, Student):
+            self.group.add(student)
+
+    def delete_student(self, last_name):
+        student = self.find_student(last_name)
+        if student:
+            self.group.remove(student)
+
+    def find_student(self, last_name):
+        for student in self.group:
+            if student.last_name == last_name:
+                return student
+        return None
+
+    def __str__(self):
+        all_students = '\n'.join(str(student) for student in self.group)
+        return f'Number: {self.number}\n{all_students}'
